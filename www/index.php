d337fb4f3b70a0162b55d8ced831b6c17a1222eb
Ivan Lyangasov <gtnhkjifhs@yandex.ru>
2019-02-13 21:25:34 +0700

    добавлено взаимодействие с БД

--- www/index.php
+++ www/index.php
@@ -7,40 +7,7 @@
  */
 ini_set('display_errors',true);
 
-
-
-//наши паблики для слежки
-$groupName = array('bot_maxim','bot_lena');
-
-initGroupName($groupName);
-
-
-//перебираем паблики
-foreach ($groupName as &$value) {
-    //запрос последнего поста
-    $jsonRes = downloadPostsFromWall($value);
-    $res = json_decode($jsonRes);
-
-    //формируем свой пост из главных данных полученного поста
-    $wallPost = array(
-        date => date('Y-m-d g:i:s', $res->response->items[0]->date),
-        domain => $value,
-        massage => $res->response->items[0]->text,
-        attachments => 'photo' . $res->response->items[0]->attachments[0]->photo->owner_id . '_' . $res->response->items[0]->attachments[0]->photo->id
-    );
-    var_dump($wallPost);
-    //пытаемся добавить запись в бд
-    $successfulTransfer = saveWallPostToDB($wallPost);
-    var_dump($successfulTransfer);
-
-    //публикуем у себя, если удалось добавление в бд
-    if($successfulTransfer) {
-        $res = uploadWallPostTowall($wallPost);
-    }
-
-    sleep(1);// = 20*кол-во пабликов (чтоб не превысить кол-во запросов в день = 5000)
-}
-//супер цикл переобхода групп в ВК
+////супер цикл переобхода групп в ВК
 //1 получили список групп для мониторинга
 //2 для каждой группы из списка проверяем дату посл. загруженного поста в БД (чтобы не дублироваться)
 //3 формируем запрос в ВК
@@ -50,12 +17,39 @@ foreach ($groupName as &$value) {
 //6 продолжаем дальше с след группой из списка
 
 
+//наши паблики для слежки
+$groupNames = array('bot_maxim','bot_lena');
+initGroupName($groupNames);
 
+//перебираем паблики
+foreach ($groupNames as &$domain) {
+    //запрос последнего поста
+    $jsonRes = downloadPostsFromWall($domain);
+    $res = json_decode($jsonRes);
+
+    //формируем свой пост из главных данных полученного поста
+    $wallPost = array(
+        date => date('Y-m-d g:i:s', $res->response->items[0]->date),
+        domainId  => getDomainId($domain),
+        massage => $res->response->items[0]->text,
+        attachments => 'photo' . $res->response->items[0]->attachments[0]->photo->owner_id . '_' . $res->response->items[0]->attachments[0]->photo->id
+    );
+
+    //пытаемся добавить запись в бд
+    $successfulTransfer = saveWallPostToDB($wallPost);
+
+    //публикуем у себя, если удалось добавление в бд
+    if ($successfulTransfer) {
+        $res = uploadWallPostTowall($wallPost);
+    }
+
+    sleep(1);// = 20*кол-во пабликов (чтоб не превысить кол-во запросов в день = 5000)
+}
 
 
 function downloadPostsFromWall($domain){
     $ownerId = NULL;
-    $offset = 1;
+    $offset = 0;
     $count = 1;//кол-во записей для получения max = 100
     $filter = "all";
     $extended = 0;
@@ -64,27 +58,35 @@ function downloadPostsFromWall($domain){
     $version = '5.37';
 
     $url = "https://api.vk.com/method/wall.get?ownerId=$ownerId&domain=$domain&offset=$offset&count=$count&filter=$filter&extended=$extended&fields=$fields&v=$version&access_token=$accessToken";
-
     $jsonResponse = file_get_contents($url);
-
     return $jsonResponse;
 }
 
-function initGroupName ($groupName){
-    foreach ($groupName as &$value) {
-        $connect = new mysqli("localhost", "root", "1511475", "posts_of_group" );//подключили бд
+function initGroupName ($groupNames){
+    foreach ($groupNames as &$domain) {
+        $connect = new mysqli("localhost", "root", "1511475", "posts_of_group");//подключили бд
         $connect->query("SET NAMES 'utf8' ");//Кодировка данных получаемых из базы
-        var_dump($groupName);
-        var_dump($value);
-        $connect->query("INSERT INTO name_of_group (domain) VALUES  ('".$value."')");
+        $connect->query("INSERT INTO name_of_group (domain) VALUES  ('$domain')");
     }
 }
+
+
 
 function saveWallPostToDB($wallPost) {
     $connect = new mysqli("localhost", "root", "1511475", "posts_of_group" );//подключили бд
     $connect->query("SET NAMES 'utf8' ");//Кодировка данных получаемых из базы
-    var_dump($connect);
-    return $connect->query("INSERT INTO post (date, massage, attachments) VALUES  ('".$wallPost[date]."', '".$wallPost[massage]."', '".$wallPost[attachments]."')");
+    if (strlen($wallPost[massage]) > 0) {
+        return $connect->query("INSERT INTO post (date, massage, attachments, name_id) VALUES  ('$wallPost[date]', '$wallPost[massage]', '$wallPost[attachments]', '$wallPost[domainId])'");
+    }
+    else {
+        return $connect->query("INSERT INTO post (date, attachments, name_id) VALUES  ('$wallPost[date]', '$wallPost[attachments]', '$wallPost[domainId]')");
+    }
+}
+
+function getDomainId($domain){
+    $connect = new mysqli("localhost", "root", "1511475", "posts_of_group" );//подключили бд
+    $connect->query("SET NAMES 'utf8' ");//Кодировка данных получаемых из базы
+    return mysqli_fetch_array($connect->query("select distinct id from name_of_group where domain like '$domain'; "))[id];
 }
 
 function uploadWallPostTowall($wallPost) {
@@ -97,8 +99,6 @@ function uploadWallPostTowall($wallPost) {
     $version = '5.37';
 
     $url = "https://api.vk.com/method/wall.post?owner_id=$ownerId&friends_only=$friendOnly&from_group=$fromGroup&message=$massage&attachments=$attachments&v=$version&access_token=$accessToken";
-    var_dump($url);
     $jsonResponse = file_get_contents($url);
-
     return json_decode($jsonResponse);
 }
